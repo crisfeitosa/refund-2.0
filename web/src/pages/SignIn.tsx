2f0648d81a0e06a669f9ba777d1f02b792b932e9
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 16:25:16 -0300

    Criando Componente de Input

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,5 +1,23 @@
+import { Input } from "../components/Input";
+
 export function SignIn() {
   return (
-    <h1>Sign In</h1>
+    <form className="w-full flex flex-col gap-4">
+      <Input
+        name="email"
+        required
+        legend="E-mail"
+        type="email"
+        placeholder="seu@email.com"
+      />
+
+      <Input
+        name="password"
+        required
+        legend="Senha"
+        type="password"
+        placeholder="123456"
+      />
+    </form>
   )
 }
