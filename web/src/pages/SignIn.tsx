4e59ece4e228cf802ea5a3f87ac60679714796d8
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 16:58:46 -0300

    Link para SignUp

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -38,6 +38,13 @@ export function SignIn() {
       <Button type="submit" isLoading={isLoading}>
         Entrar
       </Button>
+
+      <a
+        href="/signup"
+        className="text-sm font-semibold text-gray-100 mt-10 mb-4 text-center hover:text-green-800 transition ease-linear"
+      >
+        Criar Conta
+      </a>
     </form>
   )
 }
