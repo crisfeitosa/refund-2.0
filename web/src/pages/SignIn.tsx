27ead8f3627a986781957d69680037e7cd801a24
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-01 16:54:48 -0300

    Criando onSubmit com os Estados

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,26 +1,43 @@
+import React, { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    alert("Enviado!");
+  }
+
   return (
-    <form className="w-full flex flex-col gap-4">
+    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
       <Input
         name="email"
+        value={email}
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
+        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
         name="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         required
         legend="Senha"
         type="password"
         placeholder="123456"
       />
 
-      <Button type="submit">Entrar</Button>
+      <Button type="submit" isLoading={isLoading}>
+        Entrar
+      </Button>
     </form>
   )
 }
