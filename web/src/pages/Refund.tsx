31fece3ec831a9839986d4fc8a4c6f17268f5889
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-03 15:48:17 -0300

    Button Enviar

--- web/src/pages/Refund.tsx
+++ web/src/pages/Refund.tsx
@@ -3,14 +3,33 @@ import { useState } from "react";
 import { Input } from "../components/Input";
 import { Select } from "../components/Select";
 import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
 
 import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
 
 export function Refund() {
+  const [name, setName] = useState("");
+  const [amount, setAmount] = useState("");
   const [category, setCategory] = useState("");
+  const [isloading, setIsLoading] = useState(false);
+  const [fileName, setFileName] = useState<File | null>(null);
+
+  function onSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+
+    console.log({
+      name,
+      amount,
+      category,
+      fileName,
+    });
+  }
 
   return (
-    <form className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]">
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+    >
       <header>
         <h1 className="text-xl font-bold text-gray-100">
           Solicitação de reembolso
@@ -24,6 +43,8 @@ export function Refund() {
       <Input
         required
         legend="Nome da solicitação"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
       />
 
       <div className="flex gap-4">
@@ -42,11 +63,20 @@ export function Refund() {
 
         <Input
           legend="Valor"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
           required
         />
       </div>
 
-      <Upload />
+      <Upload
+        filename={fileName && fileName.name}
+        onChange={(e) => e.target.files && setFileName(e.target.files[0])}
+      />
+
+      <Button type="submit" isLoading={isloading}>
+        Enviar
+      </Button>
     </form>
   )
 }
