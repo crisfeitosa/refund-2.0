fd1aaa5d112ebb1cdbb2a1390c45eaab384a92c5
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-04 17:21:21 -0300

    formatCurrency

--- web/src/pages/Dashboard.tsx
+++ web/src/pages/Dashboard.tsx
@@ -3,7 +3,9 @@ import React, { useState } from "react";
 import { Input } from "../components/Input";
 import { Button } from "../components/Button";
 import { RefundItem } from "../components/RefundItem";
+
 import { CATEGORIES } from "../utils/categories";
+import { formatCurrency } from "../utils/formatCurrency";
 
 import SearchSvg from "../assets/search.svg";
 
@@ -11,7 +13,7 @@ const REFUND_EXAMPLE = {
   id: "123",
   name: "Cristiano",
   description: "Transporte",
-  amount: "34,50",
+  amount: formatCurrency(34.5),
   categoryImg: CATEGORIES["transport"].icon,
 }
 
