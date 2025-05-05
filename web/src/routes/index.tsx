5e1c5d44891f93120281d9fdd0929b6227472e0e
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-05 08:49:07 -0300

    Componente de Loading

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -4,12 +4,21 @@ import { AuthRoutes } from "./AuthRoutes";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
+import { Loading } from "../components/Loading";
+import { useState } from "react";
+
 export function Routes() {
+  const [isLoading] = useState(true);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     <BrowserRouter>
-      {/* <AuthRoutes /> */}
+      <AuthRoutes />
       {/* <EmployeeRoutes /> */}
-      <ManagerRoutes />
+      {/* <ManagerRoutes /> */}
     </BrowserRouter>
   )
 }
