cacd5f8d64dfce2e3fcea2d132def1bf212e0754
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-11 16:51:52 -0300

    Action

--- web/src/pages/SignIn.tsx
+++ web/src/pages/SignIn.tsx
@@ -1,34 +1,26 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
 
 export function SignIn() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
-    e.preventDefault();
-
-    alert("Enviado!");
+  function onAction(formData: FormData) {
+    console.log(formData.get("email"));
   }
 
   return (
-    <form onSubmit={onSubmit} className="w-full flex flex-col gap-4">
+    <form action={onAction} className="w-full flex flex-col gap-4">
       <Input
         name="email"
-        value={email}
         required
         legend="E-mail"
         type="email"
         placeholder="seu@email.com"
-        onChange={(e) => setEmail(e.target.value)}
       />
 
       <Input
         name="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
         required
         legend="Senha"
         type="password"
