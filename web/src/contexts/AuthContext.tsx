505b7652e048f4792ea643d10b65a4a63d9f5896
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-12 09:02:18 -0300

    Local Storage

--- web/src/contexts/AuthContext.tsx
+++ web/src/contexts/AuthContext.tsx
@@ -3,7 +3,9 @@ import { createContext, ReactNode, useState } from "react";
 type AuthContext = {
   session: null |UserAPIResponse;
   save: (data: UserAPIResponse) => void;
-}
+};
+
+const LOCAL_STORAGE_KEY = "@refund";
 
 export const AuthContext = createContext({} as AuthContext);
 
@@ -11,6 +13,9 @@ export function AuthProvider({ children }: { children: ReactNode } ) {
   const [session, setSession] = useState<null | UserAPIResponse>(null);
 
   function save(data: UserAPIResponse) {
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:user`, JSON.stringify(data));
+    localStorage.setItem(`${LOCAL_STORAGE_KEY}:token`, data.token);
+
     setSession(data);
   };
 
