9101586e7a55ff03a736e619a77edd4295ccc59a
Cristiano Feitosa <cris.immobilis@gmail.com>
2025-05-12 09:29:41 -0300

    Desconectando usuário

--- web/src/components/Header.tsx
+++ web/src/components/Header.tsx
@@ -1,20 +1,25 @@
 import LogoSvg from "../assets/logo.svg";
 import LogoutSvg from "../assets/logout.svg";
 
+import { useAuth } from "../hooks/useAuth";
+
 export function Header() {
+  const auth = useAuth();
+
   return (
     <header className="w-full flex justify-between">
       <img src={LogoSvg} alt="Logo" className="my-8" />
 
       <div className="flex items-center gap-3">
         <span className="text-sm font-semibold text-gray-200">
-          Olá, Cristiano
+          Olá, {auth.session?.user.name}
         </span>
 
         <img
           src={LogoutSvg}
           alt="Icone de sair"
           className="my-8 cursor-pointer hover:opacity-75 transition ease-linear"
+          onClick={auth.remove}
         />
       </div>
     </header>
